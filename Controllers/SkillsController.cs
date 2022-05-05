0810dad5e184cb5393caae244a3512b5d6bcff2b
Zach Robles <45700544+zdrobles@users.noreply.github.com>
2022-05-05 10:59:44 -0400

    Completed bonus missions: Reformated form page to use a table, Added new path to display completed form

--- Controllers/SkillsController.cs
+++ Controllers/SkillsController.cs
@@ -20,7 +20,7 @@ namespace SkillsTracker.Controllers
         [HttpGet("form")]
         public IActionResult SkillsForm()
         {
-            string html = "<form method='post' action='/skills/form/'><label>Date: <input type='date' name='date'></label><br>" +
+            string html = "<form method='post' action='/skills/submission/'><label>Date: <input type='date' name='date'></label><br>" +
                 "<label>C#: <select name='csharp'></label>" +
                     "<option value ='learning basics' selected>Learning basics</option>" +
                     "<option value ='making apps'>Making apps</option>" +
@@ -37,15 +37,50 @@ namespace SkillsTracker.Controllers
             return Content(html, "text/html");
         }
 
-        [HttpPost("form")]
+        [HttpPost("submission")]
         public IActionResult PostSkillsForm(string date, string csharp, string js, string py)
         {
+         //ordered list
+            //string html = $"<h1>{date}</h1>" +
+            //    "<ol>" +
+            //        $"<li>C#: {csharp}</li>" +
+            //        $"<li>JavaScript: {js}</li>" +
+            //        $"<li>Python: {py}</li>" +
+            //    "</ol>";
+
+        //2 by 3 table
+            //string html = $"<h1>{date}</h1>" +
+            //    "<table>" +
+            //        "<thead><tr>" +
+            //            "<th>C#</th>" +
+            //            "<th>JavaScript</th>" +
+            //            "<th>Python</th>" +
+            //        "</tr></thead>" +
+            //        "<tbody><tr>" +
+            //            $"<td>{csharp}</td>" +
+            //            $"<td>{js}</td>" +
+            //            $"<td>{py}</td>" +
+            //        "</tr></tbody>" +
+            //    "</table>";
+
+        //3 by 2 table
             string html = $"<h1>{date}</h1>" +
-                "<ol>" +
-                    $"<li>C#: {csharp}</li>" +
-                    $"<li>JavaScript: {js}</li>" +
-                    $"<li>Python: {py}</li>" +
-                "</ol>";
+                "<table>" +
+                    "<tr>" +
+                        "<th>C#</th>" +
+                        $"<td>{csharp}</td>" +
+                    "</tr>" +
+
+                    "<tr>" +
+                        "<th>JavaScript</th>" +
+                        $"<td>{js}</td>" +
+                    "</tr>" +
+
+                    "<tr>" +
+                        "<th>Python</th>" +
+                        $"<td>{py}</td>" +
+                    "</tr>" +                        
+                "</table>";
             return Content(html, "text/html");
         }
     }
